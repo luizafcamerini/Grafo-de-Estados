91788cfa118aaf945aacc785c8ca945a31431c83
luizafcamerini <luizacamerini@hotmail.com>
2024-05-04 17:30:56 -0300

    Inclui nó no grafo, e outras funcoes auxiliares

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ class Node():
             ocupada por um número ou por '*', que representa vazio  
         '''
         
-# a has table eh um dict:
-# hash = {
-#     'chaveHash': config
-# }
-
 
 def calculaQuadrantesAdj(posicao):
     '''
-        Função que, dado um quadrante do tabuleiro, retorna todos os quadrantes adjacentes ao mesmo dentro do vetor de config.
+        Função que, dado um quadrante do tabuleiro, retorna todos os quadrantes adjacentes 
+        ao mesmo dentro do vetor de config.
     ''' 
     quadrante = posicao + 1
     match (quadrante):
@@ -32,18 +28,54 @@ def calculaQuadrantesAdj(posicao):
         case _: return None
 
 
-def calculaHash(config):
-    mod = (10**9) + 9
-    sum = 0
-    p = 11
-    for i in range(len(config)-1):
-        sum += config[i] * (p ** i)
-        
-    return sum % mod
-
-
-def criaGrafoEstados(configInicial) -> dict:
+def calculaConfigsAdj(config) -> list:
+    lista_adjacentes = list()
     ...
 
+
+def calculaChaveHash(config):
+    return str(hash(config))
+
+
+def incluiNoGrafoEstado(config, hashtable, grafo):
+    '''
+    	1. Calcula chave hash para a nova configuração
+        2. Verifica se nó já existe no grafo por meio de sua chave
+            2.1. Se não existe, adicionamos-o na hash table
+    '''
+    node = Node(config)
+    chave_hash = calculaChaveHash(node.config)
+    if chave_hash not in hashtable.keys:
+        # Inclui na hashtable:
+        hashtable[chave_hash] = list()
+        hashtable[chave_hash] += config
+        
+        # Inclui no grafo de adjacencia
+        grafo[config] = list()
+        configuracoes_adjacentes = calculaConfigsAdj(config)
+        grafo[config] += configuracoes_adjacentes
+        
+        for config_adjacente in configuracoes_adjacentes:
+            grafo[config_adjacente].append(config)
+
+    # Retorna o grafo e hashtable atualizados:
+    return hashtable, grafo
+
+
+def criaGrafoEstados(config_inicial):
+    '''
+        1. Verifica quem são as posições adjacentes à posição vazia ('*')
+        2. Formar novos nós
+        3. Para cada nó
+            3.1. Verifica se já existe no grafo por meio da função de hash
+                3.1.1. Se não existe, adiciona-o no grafo
+                3.1.2. Chama a função recursivamente para este nó?????????????????
+    '''
+    posicao_vazia = config_inicial.index('*')
+    casas_adjacentes = calculaQuadrantesAdj(posicao_vazia)
+    for casa in casas_adjacentes:
+        # cria nó movendo quem está na casa adjacente para o vazio
+        # verifica se este novo nó existe no grafo
+        ...
 
 configInicial = Node("12345678*")
