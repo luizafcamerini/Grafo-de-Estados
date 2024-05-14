10e99a20db2dcafff81bc8a0500756ae2df5a5f6
luizafcamerini <luizacamerini@hotmail.com>
2024-05-13 22:12:08 -0300

    Comecando outro main.py

---
+++ main2.py
@@ -0,0 +1,3 @@
+# o grafo de estados NAO EH conexo!!
+# ou seja, temos que usar uma biblioteca para permutar em um vetor inicial normalmente
+# usar a hashtable para armazenar cada estado => qual a utilidade dela no programa? nao sei.
